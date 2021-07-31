38e25cb38552b739961e17655fb17142054ee8db
Dang Chau Bao Van <dangchaubaovan@gmail.com>
2021-07-31 23:40:24 +0700

    Commit Choose Car Code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'jquery/dist/jquery';
+import 'popper.js/dist/umd/popper';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -15,3 +21,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
